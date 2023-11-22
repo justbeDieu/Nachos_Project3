494a17ebab28626796d693c082acf14d8eb53ed8
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2023-11-22 17:55:48 +0700

    Update system.h

--- threads/system.h
+++ threads/system.h
@@ -31,10 +31,9 @@ extern Timer *timer;				// the hardware alarm clock
 
 #ifdef USER_PROGRAM
 #include "machine.h"
-#include "synchcons.h"
-#include "synch.h"
 extern Machine* machine;	// user program memory and registers
-extern SynchConsole* synchConsole;
+#include "synchocons.h"
+extern SynchConsole* gSynchConsole;
 #endif
 
 #ifdef FILESYS_NEEDED 		// FILESYS or FILESYS_STUB 
