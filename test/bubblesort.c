9e28b79d812add4d1486cacfc2c508a2fa9a79b1
SvvPhenex <150884314+Phenez@users.noreply.github.com>
2023-11-23 15:11:07 +0700

    Update bubblesort.c

--- test/bubblesort.c
+++ test/bubblesort.c
@@ -28,12 +28,12 @@ int main()
 	}
 
 	// Bubble sort
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n - 1; i++) {
 		for ( j = i + 1; j < n; j++) {
-			if (arr[j] > arr[j + 1]) {
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
+			if (arr[j-1] > arr[j]) {
+				temp = arr[j-1];
+				arr[j-1] = arr[j];
+				arr[j] = temp;
 			}
 		}
 	}
