6bcf83c8f6849bfd22068838af46305607ee641a
justbeDieu <dieuchiru@gmail.com>
2023-12-08 23:18:51 +0700

    Update pcb.h

--- userprog/pcb.h
+++ userprog/pcb.h
@@ -6,19 +6,28 @@
 
 class PCB {
 private:
-
+    Semaphore *joinsem;
+    Semaphore *exitsem;
+    Semaphore *mutex;
+    int exitcode;
+    Thread *thread;
+    int pid;
+    int numwait;
 
 public:
+    int parentID;
     PCB(int id);           // Initialize
     ~PCB();          // De-allocate
     int Exec(char* filename, int pID);
+    int GetNumWait();
     void JoinWait();
     void ExitWait();
     void JoinRelease();
     void ExitRelease();
     void IncNumWait();
     void DecNumWait();
-    
+    void SetExitCode(int ec);
+    int GetExitCode();
 }
 
 #endif // PCB_H
