33c78c58654dbb1c854fce5d99bee6cf6f76ec71
Hoang Dieu <135320246+justbeDieu@users.noreply.github.com>
2023-12-08 23:25:30 +0700

    Add class Semaphore

--- userprog/stable.h
+++ userprog/stable.h
@@ -3,6 +3,32 @@
 #include "synch.h"  
 #include "bitmap.h"  
 #define MAX_LOCK 10  
+
+class Semaphore {
+private: 
+    char name[50]; 
+    Semaphore *sem; // Tạo Semaphore để quản lý 
+public: 
+    // khởi tạo đối tượng Sem. Gán giá trị ban đầu là null 
+    // nhớ khởi tạo bm sử dụng 
+    Sem(char* na, int i){ 
+        strcpy(this->name,na); 
+        sem = new Semaphore(name,i); 
+    } 
+    ~Sem(){                  // hủy các đối tượng đã tạo 
+        delete sem; 
+    } 
+    void wait(){             // thực hiện thao tác chờ 
+        sem->P(); 
+    } 
+    void signal(){           // thực hiện thao tác giải phóng Semaphore 
+        sem->V(); 
+    } 
+    char* GetName(){         // Trả về tên của Semaphore 
+        return name; 
+    }
+}
+
 class STable {   
 private:  
     BitMap* bm;  
